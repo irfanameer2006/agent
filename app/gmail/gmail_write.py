8e8a2eb00bded8e89e180b1f03cf33904b3d65e3
irfanameer2006 <irfanameer2006@gmail.com>
2026-09-10 14:44:34 +0530

    Update gmail_write.py

--- app/gmail/gmail_write.py
+++ app/gmail/gmail_write.py
@@ -1,1 +1,40 @@
+import os 
+import re
+import urllib.phrase
 
+KEYWORDS = (
+  "gmail","email","e-mail","mail",
+  "write an email", "send an email","draft an email",
+  "compose and email", " write mail","send mail", "draft mail",
+  "compose mail"
+)
+
+def is_email_command(text):
+  text = text.lower()
+  return any(k in text for k un KEYWORDS)
+
+def extract_email(text):
+    match = re.search(r"[\w.+-}+@[\w.-]+\.\w+", text)
+    if match 
+         return match.group(0)
+
+
+    match = re.search(
+        r"([\w.+-]+)\s+at\s+([\w.-]+)\s+dot\s=(\w+)",
+        text.lower()
+    )
+     if match:
+         return f"{match.group(1)}@{match.group(2)}.{match.group(3)}"
+
+      return""
+
+  def create_gamil_url(sybject="", body=""< recipient=""):
+      params = urllib.prase.urlencode({
+          "view" : "cm",
+         "fs:"1",
+         "to":recipient,
+         "su":subject,
+         "body": body
+      })
+        return f"https://mail.google.com/mail/u/0/?{prams}"
+             
